b1016e06f9229c874eb6d1d8288590d47d31d2ab
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-29 23:09:08 +0100

    Fix example

--- examples/reelout_3D.jl
+++ examples/reelout_3D.jl
@@ -3,25 +3,24 @@ using KiteSimulators
 # example program that shows
 # a. how to create a video
 # b. how to create a performance plot (simulation speed vs time)
-const Model = KPS4
 
-if ! @isdefined kcu;  const kcu = KCU(se());   end
-if ! @isdefined kps4; const kps4 = Model(kcu); end
+kcu::KCU   = KCU(se())
+kps4::KPS4 = KPS4(kcu)
 
 # the following values can be changed to match your interest
-dt = 0.05
+dt::Float64 = 0.05
 TIME = 50
 TIME_LAPSE_RATIO = 5
-STEPS = Int64(round(TIME/dt))
+STEPS::Int64 = Int64(round(TIME/dt))
 STATISTIC = false
 SHOW_VIEWER = true
 SHOW_KITE = true
 SAVE_PNG  = false
-PLOT_PERFORMANCE = false
+PLOT_PERFORMANCE = true
 # end of user parameter section #
 
 if ! @isdefined time_vec; const time_vec = zeros(div(STEPS, TIME_LAPSE_RATIO)); end
-if ! @isdefined viewer; const viewer = Viewer3D(SHOW_KITE); end
+viewer::Viewer3D = Viewer3D(SHOW_KITE)
 
 # ffmpeg -r:v 20 -i "video%06d.png" -codec:v libx264 -preset veryslow -pix_fmt yuv420p -crf 10 -an "video.mp4"
 
@@ -64,9 +63,9 @@ integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.04, prn=STATISTIC)
 
 av_steps = simulate(integrator, STEPS, log=SAVE_PNG)
 if PLOT_PERFORMANCE
-    using ControlPlots
-    plot(range(0.5,TIME,step=0.5), time_vec, ylabel="CPU time [%]", xlabel="Simulation time [s]", legend=false)
-    savefig("performance.png")
+    import Plots as plots
+    plots.plot(range(0.25,TIME,step=0.25), time_vec, ylabel="CPU time [%]", xlabel="Simulation time [s]", legend=false)
+    plots.savefig("performance.png")
 end
 # mean with :Dense integrator: 6.66% CPU time, 15 times realtime
 # mean with :GMRES integrator: 1.96% CPU time, 51 times realtime
