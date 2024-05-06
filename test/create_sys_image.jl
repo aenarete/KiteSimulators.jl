6181a94684705bef08232a76b4d9f82354e46db0
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-05-06 16:08:19 +0200

    improve create_sys_image

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -11,6 +11,5 @@ PackageCompiler.create_sysimage(
     [:KiteSimulators, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=false,
-    # sysimage_build_args="-t 1",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
