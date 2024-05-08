b1b7b97b6d743bb00f4d2619397b4566bc2233f8
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-05-08 17:48:27 +0200

    update stats.jl

--- examples/stats.jl
+++ examples/stats.jl
@@ -1,5 +1,7 @@
 struct Stats
     e_mech::Float64
+    av_power::Float64
+    peak_power::Float64
     min_force::Float64
     max_force::Float64
     min_height::Float64
@@ -11,9 +13,9 @@ struct Stats
 end
 
 function show_stats(stats::Stats)
-    HEIGHT=330
+    HEIGHT=380
     UPPER_BORDER=20
-    fig = KiteViewers.GLMakie.Figure(size = (400, HEIGHT))
+    fig = GLMakie.Figure(size = (400, HEIGHT))
     if Sys.islinux()
         # sudo apt install ttf-bitstream-vera 
         lin_font="/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf"
@@ -26,11 +28,13 @@ function show_stats(stats::Stats)
         font="Courier New"
     end
     function print(lbl::String, value::String; line, font=font)
-        KiteViewers.GLMakie.text!(fig.scene, 20, HEIGHT-UPPER_BORDER-line*32; text=lbl, fontsize = 24, space=:pixel)
-        KiteViewers.GLMakie.text!(fig.scene, 250, HEIGHT-UPPER_BORDER-line*32; text=value, fontsize = 24, font, space=:pixel)
+        GLMakie.text!(fig.scene, 20, HEIGHT-UPPER_BORDER-line*32; text=lbl, fontsize = 24, space=:pixel)
+        GLMakie.text!(fig.scene, 250, HEIGHT-UPPER_BORDER-line*32; text=value, fontsize = 24, font, space=:pixel)
         line +=1    
     end
     line = print("energy:       ", @sprintf("%5.0f Wh", stats.e_mech); line = 1)
+    line = print("average power:", @sprintf("%5.0f  W", stats.av_power); line)
+    line = print("peak power:", @sprintf("%5.0f  W", stats.peak_power); line)
     line = print("min force:    ", @sprintf("%5.0f  N", stats.min_force); line)
     line = print("max force:    ", @sprintf("%5.0f  N", stats.max_force); line)
     line = print("min height:   ", @sprintf("%5.0f  m", stats.min_height); line)
@@ -40,6 +44,6 @@ function show_stats(stats::Stats)
     line = print("min az_ro:    ", @sprintf("%5.1f  °", stats.min_az_ro); line)
     line = print("max az_ro:    ", @sprintf("%5.1f  °", stats.max_az_ro); line)
 
-    display(KiteViewers.GLMakie.Screen(), fig)
+    display(GLMakie.Screen(), fig)
     nothing
 end
