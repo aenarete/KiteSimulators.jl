8f6d38ec333494f435e0ec583fac9717db71f0f7
Uwe Fechner <fechner@aenarete.eu>
2024-05-10 11:21:41 +0200

    Use .so instead of .dll

--- bin/autopilot.bat
+++ bin/autopilot.bat
@@ -7,9 +7,9 @@ for /f "delims=" %%i in ('julia --version') do set version_string=%%i
 for /f "tokens=3 delims= " %%a in ("%version_string%") do set version=%%a
 
 set julia_major=%version:~0,4%
-set image=kps-image-%julia_major%.dll
+set image=kps-image-%julia_major%.so
 
 echo Lauching KiteViewer...
 IF EXIST "bin/%image%" (
-    julia --startup-file=no  -t auto -J bin/kps-image-%julia_major%.dll --optimize=1 --project -e "include(\"./examples/autopilot.jl\");"
+    julia --startup-file=no  -t auto -J bin/kps-image-%julia_major%.so --optimize=1 --project -e "include(\"./examples/autopilot.jl\");"
 ) else julia --startup-file=no -t auto --optimize=2 --project -e "include(\"./examples/autopilot.jl\");"
--- bin/create_sys_image.bat
+++ bin/create_sys_image.bat
@@ -1,6 +1,6 @@
 @echo off
 REM This script creates a julia system image with a name that
-REM contains the Julia version, e.g. kps-image-1.7.dll
+REM contains the Julia version, e.g. kps-image-1.7.so
 REM It should be placed in the bin folder of the project.
 REM It assumes that the following files exist:
 REM test/update_packages.jl
@@ -14,10 +14,10 @@ for /f "delims=" %%i in ('julia --version') do set version_string=%%i
 for /f "tokens=3 delims= " %%a in ("%version_string%") do set version=%%a
 
 set julia_major=%version:~0,4%
-set image=kps-image-%julia_major%.dll
+set image=kps-image-%julia_major%.so
 
 IF EXIST "bin\%image%" (
-  move bin\kps-image-%julia_major%.dll bin\kps-image-%julia_major%.so.bak
+  move bin\kps-image-%julia_major%.so bin\kps-image-%julia_major%.so.bak
 )
 
 echo Updating packages...
@@ -29,5 +29,5 @@ REM julia --project -e "include(\"./test/update_packages.jl\");"
 julia --project -e "using Pkg; Pkg.activate(temp=true); Pkg.add(\"PyCall\"); Pkg.build(\"PyCall\")"
 julia --project -e "using Pkg; Pkg.precompile()"
 julia --project -e "include(\"./test/create_sys_image.jl\");"
-move kps-image_tmp.so bin\kps-image-%julia_major%.dll
+move kps-image_tmp.so bin\kps-image-%julia_major%.so
 julia --project -e "using Pkg; Pkg.precompile()"
