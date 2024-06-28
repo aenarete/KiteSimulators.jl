01e616277e1e2cac64a2e4fffa941fda0a4adcbf
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-06-28 11:56:10 +0200

    add docs

--- examples/plot_log.jl
+++ examples/plot_log.jl
@@ -3,9 +3,24 @@ using ControlPlots
 
 # the following values can be changed to match your interest
 LOG_FILE_NAME = "output/last_sim_log" # without extension!
-# end of user parameter section #
 
 include("plots.jl")
 
 log = load_log(basename(LOG_FILE_NAME), path=fulldir(LOG_FILE_NAME))
 plot_main(log)
+
+# Also available:
+#
+# plot_timing(log)
+# plot_power(log)
+# plot_control(log)
+# plot_control_II(log)
+# plot_winch_control(log)
+# plot_elev_az(log)
+# plot_elev_az2(log)
+# plot_elev_az3(log)
+# plot_side_view(log)
+# plot_side_view2(log)
+# plot_side_view3(log)
+# plot_front_view3(log)
+# plot_aerodynamics(log)
