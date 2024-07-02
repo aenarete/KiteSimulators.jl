83c202a53013b60e9b26710e393c0f7d68dfb55f
Uwe Fechner <fechner@aenarete.eu>
2024-07-02 10:58:30 +0200

    fix convert_log.jl

--- examples/convert_log.jl
+++ examples/convert_log.jl
@@ -1,10 +1,18 @@
 using KiteSimulators
 
 # the following values can be changed to match your interest
-LOG_FILE_NAME = "sim_log" # without extension!
+LOG_FILE_NAME = "output/sim_log" # without extension!
 # end of user parameter section #
 
-log_file = load_log(LOG_FILE_NAME)
+function fulldir(name)
+    if occursin("~", name)
+        return replace(dirname(name), "~" => homedir())
+    else
+        return joinpath(pwd(), dirname(name))
+    end
+end
+
+log_file = load_log(basename(LOG_FILE_NAME), path=fulldir(LOG_FILE_NAME))
 export_log(log_file)
 # log_file.name="sim_log_uncompressed"
 # save_log(log_file, false)
