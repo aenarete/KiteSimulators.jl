435a14b930599c9869662e111bbf1e7cd9c5e3f8
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-09 13:46:18 +0200

    add menu.jl

---
+++ examples/menu.jl
@@ -0,0 +1,20 @@
+using REPL.TerminalMenus
+
+function menu()
+    options = ["autopilot_ = include(\"autopilot.jl\")",
+           "quit"]
+    active = true
+    while active
+        menu = RadioMenu(options, pagesize=8)
+        choice = request("\nChoose function to execute or `q` to quit: ", menu)
+
+        if choice != -1 && choice != length(options)
+            eval(Meta.parse(options[choice]))
+        else
+            println("Left menu. Press <ctrl><d> to quit Julia!")
+            active = false
+        end
+    end
+end
+
+menu()
