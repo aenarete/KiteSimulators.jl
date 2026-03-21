80d3e7e56d12dc3bcdb6d0bfb9382867a3528fc8
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-21 23:12:52 +0100

    Cleanup

--- test_settings.jl
+++
@@ -1,7 +0,0 @@
-using KiteSimulators
-println("Loading settings...")
-set = load_settings("hydra20_600")
-println("Settings loaded. Checking delta and stiffness_factor...")
-println("delta: $(set.delta)")
-println("stiffness_factor: $(set.stiffness_factor)")
-println("Done!")
