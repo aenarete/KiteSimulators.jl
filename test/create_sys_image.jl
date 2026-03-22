954d723e032b636a59207fce14f832e30ed4d84a
Uwe Fechner <fechner@aenarete.eu>
2026-03-22 13:44:49 +0100

    Run GC.gc(true) and print free memory

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -8,6 +8,16 @@ Pkg.instantiate()
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+    if haskey(ENV, "JULIA_IMAGE_THREADS")
+        @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+    else
+        @info "JULIA_IMAGE_THREADS not defined!"
+    end
+end
+
 PackageCompiler.create_sysimage(
     [:KiteSimulators, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -2,4 +2,15 @@ __PRECOMPILE__ = true
 let    
     include("../examples/autopilot.jl")
 end
+
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+    if haskey(ENV, "JULIA_IMAGE_THREADS")
+        @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+    else
+        @info "JULIA_IMAGE_THREADS not defined!"
+    end
+end
+
 @info "Precompile script has completed execution."
